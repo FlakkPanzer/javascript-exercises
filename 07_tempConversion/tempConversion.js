d1e094ae0ee8585afc5f895c834c70efaff3f086
Erjo Guce <erjo.guce214@gmail.com>
2022-05-31 05:41:21 -0400

    Finished

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fTemp) {
+  let cTemp = Number(((5/9)*(fTemp-32)).toFixed(1));
+  return cTemp;
 };
 
-const ctof = function() {
-
+const ctof = function(cTemp) {
+  let fTemp = Number((((9/5)*cTemp) + 32).toFixed(1));
+  return fTemp;
 };
 
 // Do not edit below this line
