6ba6df3942f89285482c91fbc1a4f1ae7da8685b
Erjo Guce <erjo.guce214@gmail.com>
2022-06-01 12:25:47 -0400

    Finished

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...nums) {
+    for (let i = 0; i < nums.length; i++) {
+        let index = array.indexOf(nums[i]);
+        if (index != -1) {
+            array.splice(index, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
